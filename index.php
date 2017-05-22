ce58b8f63370b56f0dd4de4e4d4cd40c2c87d566
Negreanu Calin <negreanu0calin@gmail.com>
2017-05-22 09:50:21 +0300

    - DI

--- index.php
+++ index.php
@@ -1,40 +1,7 @@
 <?php
+require_once 'vendor/autoload.php';
+require_once 'Services/DIService.php';
 
-require __DIR__.'/vendor/autoload.php';
-const APP_NAME = 'Multidoc';
-
-use Multidoc\Factories\EnvironmentFactory;
-use Multidoc\Factories\ProjectFactory;
-
-use Multidoc\Services\FileContentParserService;
-use Multidoc\Services\InputFileService;
-use Multidoc\Services\MultidocService;
-use Symfony\Component\Console\Application;
-use Symfony\Component\Finder\Finder;
-
-$application = new Application();
-$application->add(new Multidoc\Console\ParserCommand(
-    new MultidocService(
-        new InputFileService(new Finder()),
-        new FileContentParserService(
-            new \Multidoc\Factories\AbstractFactory(
-                new ProjectFactory(new EnvironmentFactory()),
-                new \Multidoc\Factories\RouteFactory(
-                    new \Multidoc\Factories\ParameterFactory(),
-                    new \Multidoc\Factories\TagFactory()
-                ),
-                new \Multidoc\Factories\CategoryFactory()
-            )
-        ), new \Multidoc\Services\OutputFileService(
-            new \Symfony\Component\Filesystem\Filesystem(),
-            new \Multidoc\Renderers\ProjectRenderer(new \Multidoc\Renderers\EnvironmentRenderer()),
-            new \Multidoc\Renderers\CategoryRenderer(
-                new \Multidoc\Renderers\RouteRenderer(
-                    new \Multidoc\Renderers\ParameterRenderer(),
-                    new \Multidoc\Renderers\TagRenderer()
-                )
-            )
-        )
-    )
-));
-$application->run();
+$service =  \Multidoc\Services\DIService::load();
+$fac = $service->get('multidoc_service');
+var_dump($fac);
