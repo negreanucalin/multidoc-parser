be9de8d8bf2d8d3f3e74ddc1d6a5de551ce9d383
Negreanu Calin <negreanu0calin@gmail.com>
2017-05-22 09:52:15 +0300

    - del test

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once 'vendor/autoload.php';
-require_once 'Services/DIService.php';
-
-$service =  \Multidoc\Services\DIService::load();
-$fac = $service->get('multidoc_service');
-var_dump($fac);
