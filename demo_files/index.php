00d496f0d51a986ff5c53be5fe41992860d70c19
Negreanu Calin <negreanu0calin@gmail.com>
2017-06-08 10:37:18 +0300

    - Add example files

--- demo_files/index.php
+++ demo_files/index.php
@@ -16,12 +16,20 @@ $multidoc->generate(__DIR__.'\input\basic','output\basic');
 $multidoc->generate(__DIR__.'\input\basic_minimized','output\basic_minimized');
 $multidoc->generate(__DIR__.'\input\environments','output\environments');
 $multidoc->generate(__DIR__.'\input\tags','output\tags');
+$multidoc->generate(__DIR__.'\input\authorization','output\authorization');
+$multidoc->generate(__DIR__.'\input\multiple categories','output\multiple categories');
+$multidoc->generate(__DIR__.'\input\project logo','output\project logo');
+$multidoc->generate(__DIR__.'\input\files','output\files');
 
-
+//Project logo as project for ALL
 $multidoc->generate(__DIR__.'\input','output\all',array(
         __DIR__.'\input\basic\basic_project.yml',
         __DIR__.'\input\basic_minimized\basic_project.yml',
-        __DIR__.'\input\tags\basic_project.yml',
         __DIR__.'\input\environments\project.yml',
+        __DIR__.'\input\tags\project.yml',
+        __DIR__.'\input\authorization\project.yml',
+        __DIR__.'\input\multiple categories\project.yml',
+        //Project logo definition excluded here
+        __DIR__.'\input\files\project.yml',
     )
 );
